530d6ab8b4ef86ce237f21d483e073368053a181
Blade-2200 <daniel.lee.sedu@gmail.com>
2025-11-05 23:16:00 -0800

    Completed Problem
    Used Hash Map to Solve the Anagram

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int, int> seen;
-        if (s.length() == t.length()) {
-                for (int i = 0; s.length() > i; i++;) {
-                    
+        if (s.length() != t.length()) {
+            return false;
+        }
+        unordered_map<char, int> seen;
+
+        for (int i = 0; i < s.length(); i++) {
+            seen[s[i]]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            seen[t[i]]--;
+            if (seen[t[i]] < 0) {
+                return false;
             }
         }
+        return true;
     }
 };
